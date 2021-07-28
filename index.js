e5b059692cf4bf53947968930b43d607fe9b0fcf
JoeDaBu <75513610+JoeDaBu@users.noreply.github.com>
2021-07-27 20:26:40 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let myLeads = []
+let dragging, draggedOver
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -9,6 +10,7 @@ let listItens = document.querySelectorAll('.draggable');
 [].forEach.call(listItens, function(item) {
   addEventsDragAndDrop(item);
 });
+
 
 deleteBtn.addEventListener("mouseover", () => {
     deleteBtn.textContent = 'Double Click'
@@ -22,11 +24,12 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-tabBtn.addEventListener("click", function(){    
+tabBtn.addEventListener("click", function(){   
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
+        // myLeads.push(tabs[0].url)
+        // localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        // render(myLeads)
+        addItem(tabs[0].url)
     })
 })
 
@@ -35,6 +38,31 @@ tabBtn.addEventListener("click", function(){
 
 //     })
 // }
+
+function addItem(lead) {
+    myLeads.push(lead)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    let listItem =`
+                <div class="test-div">
+                    <a target='_blank' href='${lead}'>
+                        ${lead}
+                    </a>
+                </div>
+                <button class='delete'>DELETE</button>
+        `
+    let li = document.createElement('li');
+    li.setAttribute('class', 'draggable')
+    li.setAttribute('draggable', 'true')
+    li.innerHTML = listItem
+    ulEl.appendChild(li)
+    addEventsDragAndDrop(li)
+    li.children[1].addEventListener('click', () => {
+            myLeads = myLeads.filter((leadL) => leadL !== lead)
+            li.remove()
+            localStorage.clear()
+            localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    })
+}
 
 function render(leads) {
     let listItems = ""
@@ -46,7 +74,7 @@ function render(leads) {
                         ${leads[i]}
                     </a>
                 </div>
-                <button class='delete' onclick='deleteLi(this)'>DELETE</button>
+                <button class='delete'>DELETE</button>
             </li>
         `
     }
@@ -54,17 +82,26 @@ function render(leads) {
     for(let i = 0; i < leads.length; i++) {
         if (ulEl.children[i].nodeName.toLowerCase() === 'li') {
             addEventsDragAndDrop(ulEl.children[i])
+            ulEl.children[i].children[1].addEventListener('click', () => {
+                myLeads = myLeads.filter((lead) => lead !== ulEl.children[i].children[0].innerText)
+                ulEl.children[i].remove()
+                localStorage.clear()
+                localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        })
         }
     }
 }
 
-function deleteLi(button) {
-    console.log(button.parentNode.children[0].children[0].innerText)
-    myLeads = myLeads.filter((lead) => lead !== button.parentNode.children[0].children[0].innerText)
-    button.parentNode.remove()
-    localStorage.clear()
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-}
+// function deleteLi(button) {
+//     console.log(button.parentNode)
+//     button.addEventListener('click', () => {
+//         myLeads = myLeads.filter((lead) => lead !== this.parentNode.children[0].children[0].innerText)
+//         this.parentNode.remove()
+//         localStorage.clear()
+//         localStorage.setItem("myLeads", JSON.stringify(myLeads))
+//     })
+    
+// }
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
@@ -74,41 +111,52 @@ deleteBtn.addEventListener("dblclick", function() {
 
 inputBtn.addEventListener("click", function() {
     if (inputEl.value != "") {
-        myLeads.push(inputEl.value)
+        // myLeads.push(inputEl.value)
+        // localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        addItem(inputEl.value)
         inputEl.value = ""
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
+        // render(myLeads)
     }
 })
 
 function dragStart(e) {
-  this.style.opacity = '0.4';
-  dragSrcEl = this;
-  e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html', this.innerHTML);
+    setDragging(e)
+    this.style.opacity = '0.4';
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
 };
  
 function dragEnter(e) {
-  this.classList.add('over');
+    this.classList.add('over');
 }
  
 function dragLeave(e) {
-  e.stopPropagation();
-  this.classList.remove('over');
+    e.stopPropagation();
+    this.classList.remove('over');
 }
  
 function dragOver(e) {
-  e.preventDefault();
-  e.dataTransfer.dropEffect = 'move';
-  return false;
+    e.preventDefault();
+    draggedOver = e.target.children[0].innerText
+    console.log(draggedOver)
+    e.dataTransfer.dropEffect = 'move';
+    return false;
 }
  
 function dragDrop(e) {
-  if (dragSrcEl != this) {
+    const index1 = myLeads.indexOf(dragging)
+    const index2 = myLeads.indexOf(draggedOver)
+    myLeads.splice(index1, 1, draggedOver)
+    myLeads.splice(index2, 1, dragging)
+    console.log(myLeads)
+    localStorage.clear()
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    if (dragSrcEl != this) {
     dragSrcEl.innerHTML = this.innerHTML;
     this.innerHTML = e.dataTransfer.getData('text/html');
-  }
-  return false;
+    }
+    return false;
 }
  
 function dragEnd(e) {
@@ -119,7 +167,15 @@ function dragEnd(e) {
   this.style.opacity = '1';
 }
 
+const setDragging = (e) => {
+    dragging = e.target.children[0].innerText
+    console.log(dragging)
+};
+
+const test = (a,b,c) => a+b+c;
+
 function addEventsDragAndDrop(el) {
+//   el.addEventListener('drag', setDragging)
   el.addEventListener('dragstart', dragStart, false);
   el.addEventListener('dragenter', dragEnter, false);
   el.addEventListener('dragover', dragOver, false);
